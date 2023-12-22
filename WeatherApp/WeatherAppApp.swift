2860b6e32f88954abf2d24360e88290f499411c2
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2023-12-22 02:54:29 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Paulo Henrique Torres on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
